cacb311e1cc7fa43ed3827d19886b5966195ab32
Crisil Thomas <crisil971@gmail.com>
2022-11-06 20:01:19 +0530

    Added comments for modules for better readability

--- adder/data_shifter.v
+++ adder/data_shifter.v
@@ -1,3 +1,11 @@
+// A module to pipeline data based on number of LATENCY parameters
+// LATENCY - Number of pipeline stages in the path
+// Inputs:
+// 	clk, rstn
+// 	idata - Input data
+// Outputs:
+// 	odata - Output data
+
 module data_shifter #(parameter LATENCY = 1, parameter IWIDTH = 8)
 (
 	input clk,
@@ -13,19 +21,23 @@ generate
 	if (LATENCY == 0) begin
 		assign odata = idata;
 	end else begin
+		// data to hold input data and perform shifting
     reg [LATENCY*IWIDTH-1: 0] shift_register;
 	  for (m = LATENCY; m > 0; m=m-1) begin
 	  	always @(posedge clk) begin
 	  		if (!rstn)
 	  			shift_register[(m*IWIDTH - 1) -: IWIDTH] <= 0;
 	  		else begin
+					// store input data 
 	  			if (m == LATENCY)
 	  				shift_register[(m*IWIDTH - 1) -: IWIDTH] <= idata[IWIDTH-1:0];
+					// shift data based on width
 	  			else
 	  				shift_register[(m*IWIDTH - 1) -: IWIDTH] <= shift_register[((m+1)*IWIDTH - 1) -: IWIDTH];
 	  		end
 	  	end
 	  end
+		// lsb data is the shifted ouput based on latency
     assign odata = shift_register[IWIDTH-1:0];
   end
 endgenerate
